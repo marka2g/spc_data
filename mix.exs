1cf8d47633dcc381f93ba42a6926269090d63c61
Mark Sadegi <mark.sadegi@gmail.com>
2024-03-16 16:14:24 -0700

    add ecto

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule SpcData.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -22,7 +24,21 @@ defmodule SpcData.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:broadway_kafka, "~> 0.4.1"}
+      {:broadway_kafka, "~> 0.4.1"},
+      {:ecto, "~> 3.11"}
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp aliases do
+    [
+      setup: ["deps.get", "ecto.setup"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
 end
